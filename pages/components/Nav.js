37cde5b086673b0a82b19def6c000d2f6a17dfd4
shar27 <shariq.ahmed731@gmail.com>
2021-11-24 09:45:26 +0100

    new commit

--- pages/components/Nav.js
+++ pages/components/Nav.js
@@ -5,93 +5,93 @@ import Image from 'next/dist/client/image'
 
 function Nav() {
     return (
-<nav class="navbar navbar-expand-lg navbar-light bg-warning ">
-  <div class="container-fluid">
-    <a class="navbar-brand">Urban Youth</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
+<nav className="navbar navbar-expand-lg navbar-light bg-warning ">
+  <div className="container-fluid">
+    <a className="navbar-brand">Urban Youth</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarNavDropdown">
-      <ul class="navbar-nav">
+    <div className="collapse navbar-collapse" id="navbarNavDropdown">
+      <ul className="navbar-nav">
         
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle"  id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+        <li className="nav-item dropdown">
+          <a className="nav-link dropdown-toggle"  id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             About us
           </a>
-          <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+          <ul className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
           <Link href="#">
-            <li><a class="dropdown-item" >Action</a></li>
+            <li><a className="dropdown-item" >Action</a></li>
             </Link>
             <Link href="#">
-            <li><a class="dropdown-item" >Action</a></li>
+            <li><a className="dropdown-item" >Action</a></li>
             </Link>
             <Link href="#">
-            <li><a class="dropdown-item" >Action</a></li>
+            <li><a className="dropdown-item" >Action</a></li>
             </Link>
           </ul>
         </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle"  id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+        <li className="nav-item dropdown">
+          <a className="nav-link dropdown-toggle"  id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                Our programmes
           </a>
-          <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+          <ul className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
           <Link href="#">
-            <li><a class="dropdown-item" >Action</a></li>
+            <li><a className="dropdown-item" >Action</a></li>
             </Link>
             <Link href="#">
-            <li><a class="dropdown-item" >Action</a></li>
+            <li><a className="dropdown-item" >Action</a></li>
             </Link>
             <Link href="#">
-            <li><a class="dropdown-item" >Action</a></li>
+            <li><a className="dropdown-item" >Action</a></li>
             </Link>
           </ul>
         </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle"  id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+        <li className="nav-item dropdown">
+          <a className="nav-link dropdown-toggle"  id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             Our impact
           </a>
-          <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+          <ul className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
           <Link href="#">
-            <li><a class="dropdown-item" >Action</a></li>
+            <li><a className="dropdown-item" >Action</a></li>
             </Link>
             <Link href="#">
-            <li><a class="dropdown-item" >Action</a></li>
+            <li><a className="dropdown-item" >Action</a></li>
             </Link>
             <Link href="#">
-            <li><a class="dropdown-item" >Action</a></li>
+            <li><a className="dropdown-item" >Action</a></li>
             </Link>
           </ul>
         </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle"  id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+        <li className="nav-item dropdown">
+          <a className="nav-link dropdown-toggle"  id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             Work with us
           </a>
-          <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+          <ul className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
           <Link href="#">
-            <li><a class="dropdown-item" >Action</a></li>
+            <li><a className="dropdown-item" >Action</a></li>
             </Link>
             <Link href="#">
-            <li><a class="dropdown-item" >Action</a></li>
+            <li><a className="dropdown-item" >Action</a></li>
             </Link>
             <Link href="#">
-            <li><a class="dropdown-item" >Action</a></li>
+            <li><a className="dropdown-item" >Action</a></li>
             </Link>
          
           </ul>
         </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle"  id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+        <li className="nav-item dropdown">
+          <a className="nav-link dropdown-toggle"  id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             Support us
           </a>
-          <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+          <ul className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
           <Link href="#">
-            <li><a class="dropdown-item" >Action</a></li>
+            <li><a className="dropdown-item" >Action</a></li>
             </Link>
             <Link href="#">
-            <li><a class="dropdown-item" >Action</a></li>
+            <li><a className="dropdown-item" >Action</a></li>
             </Link>
             <Link href="#">
-            <li><a class="dropdown-item" >Action</a></li>
+            <li><a className="dropdown-item" >Action</a></li>
             </Link>
           </ul>
         </li>
